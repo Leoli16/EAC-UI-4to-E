a091a1abd558ac537e8b209892407a13730057e8
Leonel Abud <abudleito@gmail.com>
2025-05-14 15:57:37 -0300

    paroimpar

---
+++ Assets/Scripts/ParOimpar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ParOimpar : MonoBehaviour
+{
+    public TextMeshProUGUI resultado;
+    public TMP_InputField input;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void paroimp()
+    {
+        if (input.text == "")
+        {
+            return;
+        }
+        int num = int.Parse(input.text);
+        if (num % 2 == 0)
+        {
+            resultado.text = "PAR";
+        }
+        else
+        {
+            resultado.text = "IMPAR";
+        }
+    }
+}
